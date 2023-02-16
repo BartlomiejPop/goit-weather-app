d4ab41e682ccb0e3f0ec1fcf347b782a9b48f8e7
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-02-16 22:59:46 +0100

    commit

--- src/app.js
+++ src/app.js
@@ -1,50 +1,38 @@
-import { API_KEY } from "./config";
-
-const dataEl = document.getElementById("data");
+const dataEL = document.getElementById("data");
 const searchButtonEl = document.getElementById("search");
 const searchInputEl = document.getElementById("search-input");
 const errorMessageEl = document.getElementById("error-message");
 const tableContentEl = document.getElementById("table-content");
-const tableDataEl = document.getElementById("table-data");
 
 function getWeatherData(city) {
   errorMessageEl.textContent = "";
-  dataEl.textContent = "";
+  dataEL.textContent = "";
 
   fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
+    `http://api.weatherapi.com/v1/current.json?key=c43b11ab80144ee4bd4174150231602&q=${city}&aqi=no`
   )
     .then((response) => response.json())
     .then((data) => {
+      dataEL.textContent = JSON.stringify(data, null, 2);
       if (data.error) {
         errorMessageEl.textContent = data.error.message;
       } else {
-        dataEl.textContent = JSON.stringify(data, null, 2);
-        // dataEl.textContent = data.current.temp_c;
+        dataEL.textContent = JSON.stringify(data, null, 2);
 
         let content = "";
         for (const [key, value] of Object.entries(data.location)) {
           content += `
-            <tr>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">${key[0].toUpperCase()}${key
-            .slice(1)
-            .replaceAll("_", " ")}</td>
-                <td class="px-6 py-4">${value}</td>
-            </tr>
-          `;
+                < tr >
+                <td class="px-6 py-4">${key}</td>
+              <td class="px-6 py-4">${value}</td>
+            </tr >
+            `;
         }
-
         tableContentEl.innerHTML = content;
-        tableDataEl.classList.remove("hidden");
       }
-    })
-    .catch(() => {
-      console.log("API NIE DZIAŁA!!!!");
     });
 }
-
 searchButtonEl.addEventListener("click", () => {
   const city = searchInputEl.value;
-
   getWeatherData(city);
 });
